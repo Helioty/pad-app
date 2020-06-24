eadc908d21aebd90baac78f66c3d82943cd8a966
Hélio Filho <39842088+Helioty@users.noreply.github.com>
2020-06-24 00:04:13 -0300

    add cordova

--- src/app/services/common/common.service.ts
+++ src/app/services/common/common.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
 
-  constructor() { }
+  constructor(private alertCtrl: AlertController, private toastCtrl: ToastController) { }
+
+  // Alert's
+  async showAlert(titulo: string, msg: string) {
+    const alert = await this.alertCtrl.create({
+      header: titulo,
+      message: msg,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  // Toast's
+  async showToast(msg: string) {
+    const toast = await this.toastCtrl.create({
+      message: msg,
+      duration: 2000,
+      position: 'bottom',
+      buttons: [{
+        side: 'end',
+        icon: 'close',
+        role: 'cancel'
+      }]
+    });
+    toast.present();
+  }
+
 }
