7c72aafa9ca59496650621696ed614d1dd88eeef
Hélio Filho <39842088+Helioty@users.noreply.github.com>
2020-06-26 11:29:34 -0300

    Update to angular 10

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCH_SbSlO8U0z4dUaCX-IjcGBi9JtsUPwc',
+    authDomain: 'pad-app-8a508.firebaseapp.com',
+    databaseURL: 'https://pad-app-8a508.firebaseio.com',
+    projectId: 'pad-app-8a508',
+    storageBucket: 'pad-app-8a508.appspot.com',
+    messagingSenderId: '711682431643',
+    appId: '1:711682431643:web:6dbf78c9b3cf0787d1443e',
+    measurementId: 'G-1DK21DBFYJ'
+  }
 };
